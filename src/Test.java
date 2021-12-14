71433a2998d951402f0b7ebedf64e635eb271e10
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-14 08:37:18 +0700

    Add file

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 1000, b = 1000;
+        int c = 100, d = 100;
+        System.out.println(a == b);
+        System.out.println(c == d);
+    }
+
+}
